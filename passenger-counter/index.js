f44bcc1efd229fa0487c4397b85b7139556bc412
lharrak-botaina <lharrak.botaina@gmail.com>
2022-06-25 15:54:33 +0100

    passanger counter

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,14 +1,18 @@
-countEl = document.getElementById('count-el')
+let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
 let count =0
 
 function increment(){
-    count = count +1
+    count += 1
     console.log(count)
-    countEl.innerText= count
+    countEl.textContent= count
 
 }
 function save(){
-    console.log(count)
+    let countStr = count +" - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count= 0
 }
 
 
