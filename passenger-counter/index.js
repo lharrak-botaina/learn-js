bdd9db46f270cdc2ac91007ba573b57180154f1b
lharrak-botaina <lharrak.botaina@gmail.com>
2022-06-25 14:55:51 +0100

    add password generator

---
+++ passenger-counter/index.js
@@ -0,0 +1,14 @@
+countEl = document.getElementById('count-el')
+let count =0
+
+function increment(){
+    count = count +1
+    console.log(count)
+    countEl.innerText= count
+
+}
+function save(){
+    console.log(count)
+}
+
+
